a03b4b275287cc2715966624a7bc06d5d067b1bf
WangHaoRan <whrlmc@139.com>
2014-06-24 10:44:21 +0800

    go4

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -6,7 +6,18 @@ class IndexAction extends Action {
     }
 
     public function weibo(){
-        dump(parseSignedRequest($_POST['signed_request']));
+        //微博POST的数据
+        $weibo_post = parseSignedRequest($_POST['signed_request']);
+        if($weibo_post == '-1' || $weibo_post == '-2'){
+            $this -> assign('parse_error', 1);
+        }else if(empty($weibo_post['user_id'])){
+            $this -> assign('no_login', 1);
+        }else{
+            $this -> assign('login', 1);
+        }
+
+        dump($weibo_post);
+
         $this -> display();
     }
 }
