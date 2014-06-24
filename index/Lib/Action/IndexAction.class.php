102dc17a44cbabbf5aa3072e5909a0b280aac9c4
WangHaoRan <whrlmc@139.com>
2014-06-24 20:38:31 +0800

    222

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -18,7 +18,33 @@ class IndexAction extends Action {
             $this -> assign('login', 1);
             $this -> assign('user_id', $weibo_post['user_id']);
         }
-
         $this -> display();
+    }
+
+    public function check(){
+        $User = M('User');
+        $where = array();
+        $where['type'] = $this -> _get('type');
+        $where['content'] = $this -> _get('id');
+        $result = $User -> field('id') -> where($where) -> find();
+        $return_result = array();
+        //没有数据则新建数据
+        if(!$result){
+            $data = array();
+            $data['type'] = $this -> _get('type');
+            $data['content'] = $this -> _get('id');
+            if($id = $User -> add($data)){
+                $return_result['status'] = 'success';
+                $return_result['id'] = $id;
+            }else{
+                $return_result['status'] = 'error';
+            }
+        }else{
+            $return_result['status'] = 'success';
+            $return_result['id'] = $result['id'];
+        }
+
+        echo json_encode($return_result);
+
     }
 }
