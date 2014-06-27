dc0161a711523679c5a13fed51fd64969860dd04
WangHaoRan <whrlmc@139.com>
2014-06-28 03:19:48 +0800

    444

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -66,6 +66,8 @@ class IndexAction extends Action {
         //帮友排行
         $Num = M('Num');
         $user_rank = $Num -> alias('n') -> field('u.name as uname') -> join('nokia_user as u ON n.uid = u.id') -> order('n.sum DESC u.addtime ASC') -> limit(5) -> select();
+        echo $Num -> getLastSql();
+        dump($user_rank);
         $this -> assign('user_rank', $user_rank);
 
 
