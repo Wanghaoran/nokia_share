ebc07a7f2caabd4503f551b7cb5a0ecc8dcbecd6
WangHaoRan <whrlmc@139.com>
2014-06-28 01:44:57 +0800

    3333

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -163,4 +163,17 @@ class IndexAction extends Action {
         $this -> assign('user_rank', $user_rank);
         $this -> display();
     }
+
+    public function serververification(){
+        if(!empty($_POST['code'])){
+            //监测验证码是否有效
+            $ShareCode = M('ShareCode');
+            $where = array();
+            $where['c.code'] = $_POST['code'];
+            $result = $ShareCode -> alias('c') -> field('u.name as uname,c.code,c.status,c.orderid,c.addtime,c.checktime') -> where($where) -> join('nokia_user as u ON c.uid = u.id') -> find();
+            dump($result);
+            $this -> assign('result', $result);
+        }
+        $this -> display();
+    }
 }
